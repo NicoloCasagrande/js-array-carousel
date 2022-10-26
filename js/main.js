00ec56ffd01b5f1a83e165ee1aa6aa42b98fffb8
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2022-10-26 23:36:08 +0300

    prove javascript

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
 "use strict";
 
+const img = [];
+const slider = document.getElementById('image-container');
+
+for (let i = 0; i <= 4; i++) {
+    img[i] = `img/0${i+1}.jpg`;
+}
+
+console.log(img);
+
+for (let i = 0; i < img.length; i++) {
+    const divCreation =`<div class="slide"> 
+                            <img src="${img[i]}" alt="">
+                        </div>`;
+    slider.append(divCreation)
+}
+
+document.querySelector('.slide').classList.add('active');
+
+let active = 0;
+
+const up = document.getElementById('arrow-up');
+const down = document.getElementById('arrow-down');
+
+
+
+// up.addEventListener('click', function(){
+
+// });
+
+// down.addEventListener('click', function(){
+
+// });
+
