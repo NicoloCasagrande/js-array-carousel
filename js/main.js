ff4175513794e3963efc827660fb26ffe92c9156
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2022-10-27 00:05:59 +0300

    contatore

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ for (let i = 0; i <= 4; i++) {
 console.log(img);
 
 for (let i = 0; i < img.length; i++) {
-    const divCreation =`<div class="slide"> 
-                            <img src="${img[i]}" alt="">
-                        </div>`;
-    slider.append(divCreation)
+    const divCreation = document.createElement(`div`);
+    divCreation.classList.add('slide');
+    divCreation.innerHTML = `<img src="${img[i]}" alt="">`;
+    slider.append(divCreation);
 }
 
 document.querySelector('.slide').classList.add('active');
@@ -23,13 +23,20 @@ let active = 0;
 const up = document.getElementById('arrow-up');
 const down = document.getElementById('arrow-down');
 
+up.addEventListener('click', function(){
+    if(active===img.length - 1){
+        active = 0;
+    }else{
+        active++;
+    }
+});
 
+down.addEventListener('click', function(){
+    if(active===0){
+        active = img.length - 1;
+    }else{
+        active--;
+    }
+});
 
-// up.addEventListener('click', function(){
-
-// });
-
-// down.addEventListener('click', function(){
-
-// });
 
