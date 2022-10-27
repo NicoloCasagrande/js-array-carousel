29a8fb69216ce8c8afa8c5dbd4148985e13981b4
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2022-10-27 11:18:05 +0300

    aggiunto commenti js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 "use strict";
-
+// creo l'aray in cui dovro inserire la stringa con gli src delle immagini
 const img = [];
+// seleziono il contenitore delle mie immagini
 const slider = document.getElementById('image-container');
 
+// popolo  l'array con gli src delle immagini
 for (let i = 0; i <= 4; i++) {
     img[i] = `img/0${i+1}.jpg`;
 }
 
 console.log(img);
 
+// creo i div in cui andro a inserire le immagini
 for (let i = 0; i < img.length; i++) {
     const divCreation = document.createElement(`div`);
     divCreation.classList.add('slide');
@@ -16,14 +19,18 @@ for (let i = 0; i < img.length; i++) {
     slider.append(divCreation);
 }
 
+// seleziono il primo div e gli attribuisco la classe active
 document.querySelector('.slide').classList.add('active');
 
 let active = 0;
 
 const up = document.getElementById('arrow-up');
 const down = document.getElementById('arrow-down');
-var elements = document.getElementsByClassName("slide");
 
+// creo un array contenente tutti i div con classe slide
+var elements = document.querySelectorAll('.slide');
+
+// con questa funzione, al click della freccia up rimuovo la classe active all'elemento attivo, creo un ciclo if per azzerare il contatore se siamo sull'ultima foto e poi applico la classe active al successivo elemento
 up.addEventListener('click', function(){
     elements[active].classList.remove('active');
     if(active===img.length - 1){
@@ -34,6 +41,7 @@ up.addEventListener('click', function(){
     elements[active].classList.add('active');
 });
 
+// con questa funzione, al click della freccia up rimuovo la classe active all'elemento attivo, creo un ciclo if per azzerare il contatore se siamo sull'ultima foto e poi applico la classe active al successivo elemento
 down.addEventListener('click', function(){
     elements[active].classList.remove('active');
     if(active===0){
